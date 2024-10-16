bfdd561e4aa2f3b9a8a8e4f666d856985114b59a
vanquang2002 <quanglvhe160545@fpt.edu.vn>
2024-10-17 00:23:51 +0700

    sửa import model booking

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-
+import taxSchema from './tax.js';
+import staffSchema from './staff.js';
 const bookingSchema = new mongoose.Schema({
-    taxid: { type: mongoose.Schema.Types.ObjectId, ref: 'Tax', required: true },
+    taxid: { type: mongoose.Schema.Types.ObjectId, ref: taxSchema, required: true },
     status: { type: String, required: true },
     payment: { type: String, required: true },
-    staffid: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true },
+    staffid: { type: mongoose.Schema.Types.ObjectId, ref: staffSchema, required: true },
     price: { type: Number, required: true },
     checkin: { type: Date, required: true },
     note: { type: String },
